fd3ce86a9fd6621500859376c09e50282c7546a7
Lucas <lucasesteche13@gmail.com>
2024-08-05 12:31:47 -0300

    Se creo la conexión a la base de datos

--- database.js
+++ database.js
@@ -1,1 +1,15 @@
-const database = (req)
+const taskdb = require(mysql2)
+
+
+async function connectiondataabase(params) {
+    
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'tasks'
+}) 
+return connection
+};
+
+module.exports = {connectiondataabase}
