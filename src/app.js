479d3bce6a3931a7bd0beeffcf53ab0d4f67e523
Lucas <lucasesteche13@gmail.com>
2024-08-07 11:41:31 -0300

    Se agregó las operaciones de agregar y eliminar

--- src/app.js
+++ src/app.js
@@ -13,4 +13,42 @@ app.get('/task', async (req, res) => {
 });
 
 
-app.listen(3000, () => console.log('servidor corriendo en el puerto 3000'));
+
+// Agregar tareas 
+app.post('/task', async (req, res) => {
+   const connection = await newConnection();
+   const { title, description, isComplete } = req.body;
+   const [result] = await connection.query("INSERT INTO tasks (title, description, isComplete) VALUES (?, ?, ?)", [title, description, isComplete]);
+ 
+   if (result.affectedRows === 0) {
+     res.status(500).json({ message: "No es posible añadir la taréa" });
+   } else {
+     res.status(201).json({ message: "La tarea se agrego correctamente", id: result.insertId, title, description, isComplete });
+   }
+ 
+   connection.end();
+ });
+ 
+
+// Eliminar una tarea
+app.delete('/task/:id', async (req, res) => {
+   const connection = await newConnection();
+   const { id } = req.params;
+ 
+   try {
+     const [result] = await connection.query("DELETE FROM tasks WHERE id = ?", [id]);
+ 
+     if (result.affectedRows === 0) {
+       res.status(404).json({ message: "No se encontró la tarea con el ID proporcionado" });
+     } else {
+       res.status(200).json({ message: "La tarea se eliminó correctamente", id });
+     }
+   } catch (error) {
+     res.status(500).json({ message: "Error al eliminar la tarea", error: error.message });
+   } finally {
+     connection.end();
+   }
+ });
+
+
+app.listen(3000, () => console.log('servidor corriendo en el puerto 3000'))
